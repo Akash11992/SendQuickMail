17467b357b533c78002fc50fe491b90e5818187a
Akash11992 <117894322+Akash11992@users.noreply.github.com>
2023-03-31 12:42:52 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  // apiUrl:'https://zoho-test.onrender.com',
+  apiUrl:'https://example-djer.onrender.com',
+  
+  apiMockUrl:'/api',
+  domainUrl:'http://localhost:5000/',
+  appEnv: 'production',
+  token:'RANDOM_GUID'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl:'https://example-djer.onrender.com',
+  apiMockUrl:'/api',
+  domainUrl:'http://localhost:5000/',
+  appEnv: 'development',
+  token:'RANDOM_GUID'
+};
